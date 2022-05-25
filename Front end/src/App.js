1c6a1550388952218d23aa0c8ef56f3fb99162dc
ofeknissan <44815100+ofeknissan@users.noreply.github.com>
2022-05-25 20:22:49 +0300

    Add files via upload

---
+++ Front end/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter,Route, Routes} from 'react-router-dom'
+import Signup from './Component/Signup/Signup'
+import SignIn from "./Component/SignIn/SignIn";
+import Chat from "./Component/Chat/Chat";
+import Geo from "./Component/Geo/Geo"
+
+function App() {
+  return (
+    <>
+    
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<SignIn/>}/>
+              <Route path="/signup" element={<Signup/>}/>
+              <Route path="/signin" element={<SignIn/>}/>
+              <Route path="/chat" element={<Chat/>}/>
+              <Route path="/geo" element={<Geo/>}/>
+          </Routes>
+          </BrowserRouter>
+
+    </>
+    );
+}
+
+export default App;
