a3bcb15d3d1b9462e800c794daf496709500aa37
ofeknissan <44815100+ofeknissan@users.noreply.github.com>
2022-05-25 20:53:49 +0300

    Reupload components

---
+++ Front end/src/Component/ImageMessage/ImageMessage.js
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+import "./ImageMessage.css";
+function stringGen(len) {
+    var text = "";
+    
+    var charset = "abcdefghijklmnopqrstuvwxyz";
+    
+    for (var i = 0; i < len; i++)
+      text += charset.charAt(Math.floor(Math.random() * charset.length));
+    
+    return text;
+  }
+const ImageMessage = (props) => {
+    const [id,setId] = useState(stringGen(7));
+    return (
+      <>
+        <div id={props.id} className={`${props.isSender && "ms-auto"} bubble-wrapper`}>
+          <div
+            className={`d-inline-flex flex-column p-2 ms-2 mb-2 bd-highlight ${
+              !props.isSender && "Imessage-reciever"
+            } ${props.isSender && "Imessage-sender"}`}
+          >
+            <div className="left-point"></div>
+            <img
+              data-bs-toggle="modal"
+              data-bs-target={'#'+id}
+              className="img-message"
+              src={props.children}
+            />
+            <div className="ms-auto message-time">{props.time}</div>
+            <div></div>
+          </div>
+        </div>
+        <div
+          className="modal fade"
+          id={id}
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content modal-current-contact">
+              <div className="modal-body">
+                <center>
+                  <img src={props.children} className="message-img" />
+                </center>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+};
+export default ImageMessage;
---
+++ Front end/src/Component/UserDeatils/UserDeatils.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./UserDeatils.css";
+import ContactModal from "../ContactModal/ContactModal.js";
+
+const UserDeatils = (props) => {
+  return (
+    <div className="d-flex text-light bd-highlight align-items-center user-details-box">
+      <div className="p-2 user-img-div">
+        <img className="user-img" src={props.img} ></img>
+      </div>
+      <div className="p-2 bd-highlight name">{props.children}</div>
+      <ContactModal onsubmit={props.onsubmit}></ContactModal>
+    </div>
+  );
+};
+
+export default UserDeatils;
